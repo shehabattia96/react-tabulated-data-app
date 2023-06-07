cf419d24ad3c6beae3216e3637b087db929363f0
Shehab Attia <shehabattia96@gmail.com>
2023-06-07 17:23:25 -0400

    Fixed typescript webpack not loading from outside rootdir.

--- webapp/config-overrides.js
+++ webapp/config-overrides.js
@@ -1,12 +1,14 @@
-const { override, useBabelRc, removeModuleScopePlugin, addExternalBabelPlugin, addWebpackAlias } = require('customize-cra')
+const { override, removeModuleScopePlugin, addExternalBabelPlugin, addWebpackAlias, addBabelPresets, babelInclude } = require('customize-cra')
 const path = require('path')
 
 module.exports = override(
-    useBabelRc(),
-    addWebpackAlias({
-        ['@app']: path.resolve(__dirname, '..')
-    }),
+    addBabelPresets([
+        '@babel/preset-typescript',]),
     addExternalBabelPlugin(['@babel/plugin-syntax-import-assertions',
         { allowNamespaces: true }]),
     removeModuleScopePlugin(),
+    addWebpackAlias({
+        ['@app']: path.resolve(__dirname, '..')
+    }),
+    babelInclude([path.resolve(__dirname, 'src'), path.resolve(__dirname, '../mock'), path.resolve(__dirname, '../core')])
 )
